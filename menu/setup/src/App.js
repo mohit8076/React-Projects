3a5d11667d751674e80ca11cb7780729d9802117
mohit8076 <mynkchauhan1234@gmail.com>
2021-12-10 10:10:43 +0530

    Projects-Created

---
+++ menu/setup/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+const allCategories =['all',...new Set(items.map((item) => item.category))]
+
+function App() {
+  const [item,SetItem] = useState(items)
+  const [categories,setCategories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    if(category === 'all'){
+      SetItem(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    SetItem(newItems);
+  }
+  return (
+    <main>
+      <section className= 'menu section'>
+        <div className = 'title'>
+          <h2>our menu</h2>
+          <div className ='underline'></div>
+        </div>
+        <Categories categories ={categories} filterItems = {filterItems}/>
+        <Menu items ={item}/>
+      </section>
+    </main>
+  )
+}
+
+export default App;
